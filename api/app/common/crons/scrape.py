08bf8f88ae95f5abb085862af805c00add0d9370
Kestuts Kazlauskas <kestutis.kazlauskas@osos.lt>
2018-03-06 22:37:37 +0200

    count channel all videos median on scrape cronjob

--- api/app/common/crons/scrape.py
+++ api/app/common/crons/scrape.py
@@ -2,6 +2,7 @@ from datetime import timedelta,datetime
 from sqlalchemy.dialects.mysql import insert
 from sqlalchemy.orm import load_only
 from sqlalchemy.sql import text
+from statistics import median
 
 from app import db
 from app.common.helpers.common import is_string
@@ -71,7 +72,7 @@ class YoutubeChannelScrapeJob:
             hour = now - timedelta(hours=1)
             hour_range = now - timedelta(hours=1, minutes=range_)
 
-            if hour > video['publishedAt'] > hour_range:
+            if hour > video['published'] > hour_range:
                 # get statics and count and video
                 statistic = Statistic.query.filter(
                     Statistic.video_id == video['youtube_id']
@@ -80,7 +81,7 @@ class YoutubeChannelScrapeJob:
 
                 if statistic:
                     # liner interpolation method for getting first hour views
-                    interval = (video['publishedAt'] + timedelta(hours=1)) - statistic.time_created
+                    interval = (video['published'] + timedelta(hours=1)) - statistic.time_created
                     interval = int((interval.total_seconds() % 3600) // 60)
 
                     views = video['views'] - statistic.views
@@ -103,8 +104,17 @@ class YoutubeChannelScrapeJob:
         db.session.commit()
 
     @staticmethod
-    def _count_channel_median():
-        pass
+    def _count_channel_median(channel_id):
+        """Method for counting median of all chanel videos first_hours_views"""
+        videos = Video.query.options(load_only('first_hour_views'))\
+            .filter(Video.first_hour_views != None).all()
+
+        if videos:
+            first_views = [video.first_hour_views for video in videos]
+
+            db.session.query(Channel).filter(
+                Channel.id == channel_id
+            ).update({'views_median': median(first_views)})
 
     def run(self):
         """Method for running crontjob of youtube channel videos scraping"""
@@ -130,4 +140,5 @@ class YoutubeChannelScrapeJob:
                     self._count_first_hour_view(videos, self.range)
                     self._insert_statistics(videos)
 
-            self._count_channel_median()
+            self._count_channel_median(channel.id)
+        db.session.commit()
